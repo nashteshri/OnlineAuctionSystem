1a1648d130f0cf24007a054c72ca5ad6afe071c4
SHRI_7 <nashteshrinivas@gmail.com>
2025-03-28 01:12:45 +0530

    backend and frontend connected

--- frontend/src/app/auction/auction.service.ts
+++ frontend/src/app/auction/auction.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuctionService {
+  private apiUrl = 'http://localhost:3000/api/auctions'; // Backend API URL
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // Create a new auction
+  createAuction(auctionData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}`, auctionData);
+  }
+
+  // Get all auctions
+  getAllAuctions(): Observable<any> {
+    return this.http.get(`${this.apiUrl}`);
+  }
+
+  // Get auction by ID
+  getAuctionById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  // // Search auctions by keyword
+  // searchAuctions(keyword: string): Observable<any> {
+  //   return this.http.get(`${this.apiUrl}/search?query=${keyword}`);
+  // }
+
+  // // Delete auction by ID
+  // deleteAuction(id: string): Observable<any> {
+  //   return this.http.delete(`${this.apiUrl}/${id}`);
+  // }
 }
--- frontend/src/app/auction/create-auction/create-auction.component.ts
+++ frontend/src/app/auction/create-auction/create-auction.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { AuctionService } from '../auction.service';
 
 @Component({
   selector: 'app-create-auction',
@@ -10,7 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateAuctionComponent implements OnInit {
   auctionForm!:FormGroup;
-  constructor(private fb:FormBuilder){}
+  constructor(private fb:FormBuilder ,private auctionservice:AuctionService){}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties
@@ -30,12 +30,17 @@ export class CreateAuctionComponent implements OnInit {
 
   onSubmit() {
     if (this.auctionForm.valid) {
-      console.log('Auction Created:', this.auctionForm.value);
-      alert('Auction Created Successfully!');
-      this.auctionForm.reset();
-    } else {
-      alert('Please fill out the form correctly.');
-    }
+      this.auctionservice.createAuction(this.auctionForm.value).subscribe({
+        next:(res)=>console.log('Auction Created:',res),
+        error:(err)=> console.error('error:',err)
+
+      })
+      
+    //   alert('Auction Created Successfully!');
+    //   this.auctionForm.reset();
+    // } else {
+    //   alert('Please fill out the form correctly.');
+     }
   }
 
 }
